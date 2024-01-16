35b910407fe0a376be50adc2f11620d9eabdfc63
Alexandr <alexandr-troshin>
2024-01-17 02:03:15 +0400

    Создал класс Поток содержащий в себе список УчебныхГрупп и реализующий интерфейс Iterable

---
+++ O.R.Pr_3/src/java/org/example/oop/StreamComparator.java
@@ -0,0 +1,12 @@
+package org.example.oop;
+
+import java.util.Comparator;
+
+public class StreamComparator implements Comparator<StudentGroup3> {
+
+
+    @Override
+    public int compare(StudentGroup3 o1, StudentGroup3 o2) {
+        return Integer.compare(o1.getStudentsList().size(), o2.getStudentsList().size());
+    }
+}
